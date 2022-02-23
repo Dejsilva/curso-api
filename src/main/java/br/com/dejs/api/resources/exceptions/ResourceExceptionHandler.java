9220cd1e33dc1dcdfbaa740cc9893a49d07eeb09
Dejsilva <dejs_silva@hotmail.com>
2022-02-23 14:31:21 -0300

    tratamento de excecao para objeto não encontrado

---
+++ src/main/java/br/com/dejs/api/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package br.com.dejs.api.resources.exceptions;
+
+import br.com.dejs.api.services.exceptions.ObjectNotFoundExceptions;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundExceptions.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundExceptions ex, HttpServletRequest request) {
+
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+
+}
+
--- src/main/java/br/com/dejs/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dejs/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.dejs.api.services.impl;
 import br.com.dejs.api.domain.User;
 import br.com.dejs.api.repositories.UserRepository;
 import br.com.dejs.api.services.UserService;
+import br.com.dejs.api.services.exceptions.ObjectNotFoundExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundExceptions("Objeto não encontrado"));
     }
 }
